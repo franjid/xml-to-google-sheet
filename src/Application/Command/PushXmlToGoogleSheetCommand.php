e6649e590ed43d0485936b85185ddab84e65a903
Fran Iruela <franjid@gmail.com>
2020-12-07 18:06:04 +0100

    Handle/Log errors

--- src/Application/Command/PushXmlToGoogleSheetCommand.php
+++ src/Application/Command/PushXmlToGoogleSheetCommand.php
@@ -8,6 +8,8 @@ use App\Domain\Service\XmlDataMapper;
 use App\Infrastructure\FileReader\FileReaderFactory;
 use Exception;
 use InvalidArgumentException;
+use JsonException;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,16 +26,19 @@ class PushXmlToGoogleSheetCommand extends Command
     private XmlDataMapper $xmlDataMapper;
     private XmlExporterInterface $xmlExporter;
     private FileReaderFactory $fileReaderFactory;
+    private LoggerInterface $logger;
 
     public function __construct(
         XmlDataMapper $xmlDataMapper,
         XmlExporterInterface $xmlExporter,
-        FileReaderFactory $fileReaderFactory
+        FileReaderFactory $fileReaderFactory,
+        LoggerInterface $logger
     )
     {
         $this->xmlDataMapper = $xmlDataMapper;
         $this->xmlExporter = $xmlExporter;
         $this->fileReaderFactory = $fileReaderFactory;
+        $this->logger = $logger;
 
         parent::__construct();
     }
@@ -55,25 +60,53 @@ class PushXmlToGoogleSheetCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('# XML to Google API exporter');
+        $output->writeln('<info>XML to Google API exporter</info>');
 
-        $output->writeln('Getting XML content...');
-        $fileContent = $this->getFileContent($input);
+        $output->writeln('<comment>Getting XML content...</comment>');
+        try {
+            $fileContent = $this->getFileContent($input);
+        } catch (Exception $e) {
+            $this->logger->error('Error getting XML content', [$e->getMessage()]);
 
+            return Command::FAILURE;
+        }
+
+        $output->writeln('<comment>Transforming XML...</comment>');
         try {
             libxml_use_internal_errors(false);
             $xmlObject = simplexml_load_string($fileContent, null, LIBXML_NOCDATA);
         } catch (Exception $e) {
-            die(libxml_get_last_error()->message);
+            $this->logger->error(
+                'Error transforming XML string to SimpleXMLElement object',
+                [
+                    $e->getMessage(),
+                    libxml_get_last_error()->message
+                ]
+            );
+
+            return Command::FAILURE;
         }
 
-        $mappedXml = $this->xmlDataMapper->map($xmlObject);
-        $output->writeln('Pushing content to Google Sheet...');
-        $sheetId = $this->xmlExporter->export($mappedXml);
+        $output->writeln('<comment>Mapping XML...</comment>');
+        try {
+            $mappedXml = $this->xmlDataMapper->map($xmlObject);
+        } catch (JsonException $e) {
+            $this->logger->error('Error mapping XML headers and data (malformed?)', [$e->getMessage(),]);
 
-        $output->writeln('https://docs.google.com/spreadsheets/d/' . $sheetId);
+            return Command::FAILURE;
+        }
 
-        return 0;
+        $output->writeln('<comment>Pushing content to Google Sheet...</comment>');
+        try {
+            $sheetId = $this->xmlExporter->export($mappedXml);
+            $output->writeln('<info>https://docs.google.com/spreadsheets/d/' . $sheetId . '</info>');
+
+            return Command::SUCCESS;
+        } catch (Exception $e) {
+            $this->logger->error('Error pushing data to Google Spreadsheet', [$e->getMessage()]);
+
+            return Command::FAILURE;
+        }
     }
 
     private function getFileContent(InputInterface $input): string
